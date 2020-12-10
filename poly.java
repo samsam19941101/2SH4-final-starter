bb4dc1be0963431079d1d34e827e90163ec2badd
Donglin Wu <71463191+samsam19941101@users.noreply.github.com>
2020-12-10 17:29:15 -0500

    Update poly.java
    finalupdate

--- poly.java
+++ poly.java
@@ -55,19 +55,19 @@ NOTE that this is a code snippet and not a complete java program; hence, you do 
 */
 
 var1.method2();
-//answer: Mammal 3
+//answer: Mammal 2
 
 var2.method2();
 //answer: Horse 2
 
 var3.method2();
-//answer: Ponny 2
+//answer: Ponny 2  Mammal 2
 
 var4.method2();
 //answer: Horse 2
 
 var5.method2();
-//answer: error
+//answer: error  because objective is not defined
 
 var6.method2();
 //answer: Creature 2
@@ -77,40 +77,41 @@ var1.method3();
 
 
 var2.method3();
-//answer:   Horse 2  Mammal 3
+//answer:   error bacause there is not method 3 for var2
 
 var3.method3();
-//answer:   Ponny 2
+//answer:   Ponny 2  Mammal 2  Mammal 3
+
 
 var4.method3();
-//answer:
+//answer: Horse 2  Mammal 3
 
 ((Ponny) var1).method1();
-//answer:
+//answer: error because  Mammal cannot be cast to Ponny
 
 ((Horse) var2).method1();
-//answer:
+//answer: Horse 1
 
 ((Ponny) var3).method1();
-//answer:
+//answer:Ponny 1
 
 ((Ponny) var4).method1();
-//answer:
+//answer:error because Horse cannot be cast to Ponny
 
 ((Horse) var4).method2();
-//answer:
+//answer:  Horse 2
 
 ((Mammal) var5).method2();
-//answer:
+//answer:  Mammal 2
 
 ((Creature) var5).method3();
-//answer:
+//answer:error becuase Objective is not definded
 
 ((Horse) var2).method3();
-//answer:
+//answer:   Horse 2    Mammal 3
 
 ((Ponny) var3).method3();
-//answer:
+//answer:  Ponny 2  Mammal 2  Mammal 3
 
 ((Ponny) var5).method3();
-//answer:
+//answer: error because Mammal cannot be cast to Ponny
